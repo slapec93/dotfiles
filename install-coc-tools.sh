6ccfef5ae17a46aa80a8ce8410104ee51cff15c7
Gergely Bekesi <gergely.bekesi@bitrise.io>
2020-02-25 20:29:06 +0100

    Add setup files

---
+++ install-coc-tools.sh
@@ -0,0 +1,1 @@
+vim -c 'CocInstall -sync coc-json coc-go coc-pairs coc-solargrapgh |q'
---
+++ setup.sh
@@ -0,0 +1,2 @@
+ln -s ~/develop/dotfiles/vimrc ~/.vimrc
+ln -s ~/develop/dotfiles/vim/ftplugin/ ~/.vim/ftplugin
